874146a8d97d8a8bbea8e9df9a3b0374a95fc14e
Fabien Fontaine <Fabien Fontaine>
2017-02-07 16:18:50 +0100

    step=1

--- td2/app/listesController.js
+++ td2/app/listesController.js
@@ -43,11 +43,7 @@ choixMultController.controller('choixMultController', function($http){
     ]; 
 	
 	//Tableau des produits à inclure
-	this.includedItems = [{
-            "url": "http://tutorialzine.com/2013/07/50-must-have-plugins-for-extending-twitter-bootstrap/",
-            "title": "50 Must-have plugins for extending Twitter Bootstrap",
-            "image": "http://cdn.tutorialzine.com/wp-content/uploads/2013/07/featured_4-100x100.jpg"
-        }]; 
+	this.includedItems = []; 
 
 	 //Tableau des éléments sélectionnés dans la liste des produits disponibles
 	this.selectedDispoItems = [];
@@ -60,30 +56,39 @@ choixMultController.controller('choixMultController', function($http){
 	
 	//Ajoute les produits disponibles en surbrillance (selectedDispoItems) à la liste des produits à inclure (includedItems)
 	this.addToIncluded = function(){
-        console.log("passe ici");
-        console.log(self.selectedDispoItems);
         angular.forEach(self.selectedDispoItems,function(element){
-            console.log("passe ici");
-            nb = self.dispoItems.indexOf(element);
-            self.includedItems.push(self.dispoItems[nb]);
-            self.dispoItems.splice(nb,1);
+            i = self.dispoItems.indexOf(element); 
+            self.includedItems.push(self.dispoItems[i]);
+            self.dispoItems.splice(i,1);
         });
 
 	}
 
 	//Ajoute tous les produits disponibles (dispoItems) à la liste des produits à inclure (includedItems)
 	this.addAllToIncluded = function(){
-
+        while(self.dispoItems.length!=0){
+            self.includedItems.push(self.dispoItems[0]);
+            self.dispoItems.splice(0,1); //supprime le 1er une fois
+        }
 	}
 
 	//Retire les produits sélectionnés en surbrillance (selectedIncludedItems)
 	// pour les remettre dans la liste des produits disponibles (items)
 	this.removeFromIncluded = function(){
-
+        console.log(self.selectedIncludedItems);
+        angular.forEach(self.selectedIncludedItems,function(element){
+            i = self.includedItems.indexOf(element); 
+            self.dispoItems.push(self.includedItems[i]);
+            self.includedItems.splice(i,1);
+        });
 	}
 
 	//Retire tous les produits à inclure (includedItems) pour les remettre dans la liste des produits disponibles (items) 
 	this.RemoveAllFromIncluded =function(){
+        while(self.includedItems.length!=0){
+            self.dispoItems.push(self.includedItems[0]);
+            self.includedItems.splice(0,1); //supprime le 1er une fois
+        }
 
 	}
 
