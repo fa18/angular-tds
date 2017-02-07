583da55127553b171c9bb6de26fe713addcf7220
Fabien Fontaine <Fabien Fontaine>
2017-02-07 17:04:10 +0100

    preparation

---
+++ td2/ex2/app/contactController.js
@@ -0,0 +1,40 @@
+
+//Tableau des contacts existants
+this.contacts = [] ;
+
+//Variable contenant le contact à modifier
+this.contact;
+
+//Variable contenant le contact modifié par le formulaire
+this.tmpContact;
+
+//Chaîne indiquant la modification ou l'ajout d'un contact
+this.operation;
+
+//Contrôle l'affichage du formulaire d'ajout/modification
+this.edit;
+
+//Affiche le formulaire de modification du contact
+function = this.toUpdate(contact){
+
+}
+
+//Affiche le formulaire d'ajout d'un contact
+function = this.toAdd(){
+
+}
+
+//Ajoute le contact
+function = this.add(){
+
+}
+
+//Met à jour la liste des contacts après validation du formulaire (ajout ou modif)
+function = this.update(){
+
+}
+
+//Supprime de la liste le contact
+function = this.delete(contact){
+	
+}
